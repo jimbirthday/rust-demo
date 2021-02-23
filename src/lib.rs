062fdaf5f424f2c6f0a44266d05404a92c2e6667
jim <303600370@qq.com>
2021-02-23 16:03:33 +0800

    a

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 extern crate serde_yaml;
 extern crate yaml_rust;
 
-use std::{hash::Hash, ops::Index, result::Result};
-
-use std::collections::BTreeMap;
-use yaml_rust::{YamlEmitter, YamlLoader};
+use std::{collections::HashMap, result::Result};
 mod model;
 
 pub fn parse_yml(s: &str) -> Result<model::DAG, serde_yaml::Error> {
@@ -13,51 +10,52 @@ pub fn parse_yml(s: &str) -> Result<model::DAG, serde_yaml::Error> {
 }
 
 #[allow(dead_code)]
-pub fn parse_yml1(s: &str) -> Result<BTreeMap<String, String>, serde_yaml::Error> {
-    let docs = YamlLoader::load_from_str(	"# 结构概览\nversion:\n  type: enum   # string,enum,mixed\n  rule: [ \"1.0\" ]\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n\ntriggers:\n  type: enum\n  rule: [ 'commitMessage','push','pr' ]\n  displayName: 触发规则\n  description: 触发规则\n  required: true\n\nname:\n  type: string\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 流水线唯一ID\n  description: 流水线唯一ID的描述\n  required: true\n\ndisplayName:\n  type: string\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 流水线名称\n  description: 流水线名称的描述\n  required: true\n\njob:\n  type: regexp\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n\njobs:\n  type: list\n  rule:\n    mix: 0\n    max: 100\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n  message: # 错误提示的文本设定\n    require: \"{$s} 必须 {$s}\"\n    length: 长度必须为 {$lenght}\n\nstages:\n  type: mixed\n  rule:\n    - string:\n        length: '^\\d{1,10}$'\n    - list:\n        mix: 0\n        max: 100\n  description: 流水线阶段的描述\n  required: true");
-    assert_eq!(docs.is_ok(), true);
-    let docs = docs.unwrap();
-    for v in docs.iter() {
-        println!("1");
-        // println!("doc:------  {:?}", v);
-        for av in v.as_hash() {
-            for (_k, avv) in av{
-                for avvvv in avv.as_hash() {
-                    for (k,bv) in avvvv{
-                        println!("k : ==== {:?}",k);
-                        println!("bv : ==== {:?}",bv);
+pub fn parse_yml_to_value(v: serde_yaml::Value) -> Result<model::Detail, serde_yaml::Error> {
+    let res: model::Detail = serde_yaml::from_value(v)?;
+    Ok(res)
+}
+
+fn value_to_detail(s: &str) -> Result<serde_yaml::Value, serde_yaml::Error> {
+    let res: serde_yaml::Value = serde_yaml::from_str(s)?;
+    Ok(res)
+}
+
+pub fn get_dag(s: &str) {
+    let mut res = HashMap::new();
+    let a = value_to_detail(s);
+    match a {
+        Err(e) => {
+            println!("parse_yml_to_value err :{:?}", e);
+        }
+        Ok(r) => {
+            println!("parse_yml_to_value ok : {:?}", r);
+            match r {
+                serde_yaml::Value::Mapping(v) => {
+                    for (key, value) in v {
+                        match key {
+                            serde_yaml::Value::String(s) => {
+                                res.insert(s, value);
+                            }
+                            _ => println!("unkown type: {:?}", key),
+                        }
                     }
                 }
+                _ => println!("unkown type : {:?}", r),
             }
         }
-        println!("2");
-    }
-    // let mut out_str = String::new();
-    // let mut emitter = YamlEmitter::new(&mut out_str);
-    // if let Err(e) = emitter.dump(doc) {
-    //     println!("cccc ----- {:?}", e);
-    // }
-    let res: BTreeMap<String, String> = serde_yaml::from_str(s)?;
-    Ok(res)
+    };
+    println!("rules o:{:?}", res);
 }
 
 #[test]
 fn test_yaml() {
-    let s = "version:\n  type: enum   # string,enum,mixed\n  rule: [ \"1.0\" ]\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true";
-    match parse_yml(s) {
+    let s =  "# 结构概览\nversion:\n  type: enum   # string,enum,mixed\n  rule: [ \"1.0\" ]\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n\ntriggers:\n  type: enum\n  rule: [ 'commitMessage','push','pr' ]\n  displayName: 触发规则\n  description: 触发规则\n  required: true\n\nname:\n  type: string\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 流水线唯一ID\n  description: 流水线唯一ID的描述\n  required: true\n\ndisplayName:\n  type: string\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 流水线名称\n  description: 流水线名称的描述\n  required: true\n\njob:\n  type: regexp\n  rule:\n    length: '^\\d{1,10}$'\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n\njobs:\n  type: list\n  rule:\n    mix: 0\n    max: 100\n  displayName: 配置描述版本号\n  description: 配置描述文件版本号,目前只有 1.0\n  required: true\n  message: # 错误提示的文本设定\n    require: \"{$s} 必须 {$s}\"\n    length: 长度必须为 {$lenght}\n\nstages:\n  type: mixed\n  rule:\n    - string:\n        length: '^\\d{1,10}$'\n    - list:\n        mix: 0\n        max: 100\n  description: 流水线阶段的描述\n  required: true";
+    match model::DAG::from_str(s) {
         Err(e) => {
-            println!("err:{:?}", e);
+            println!("err :{:?}", e);
         }
-        Ok(r) => {
-            println!("dag ok: {:?}", r);
-            match &r.version.rule{
-                serde_yaml::Value::Sequence(v)=>{
-                    for o in v{
-                        println!("rules o:{:?}",o)
-                    }
-                },
-                _=>println!("unkown type")
-            }
+        Ok(t) => {
+            println!("dag :{:?}", t);
         }
     };
 }
--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,52 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
-#[derive(Debug,  Serialize, Deserialize)]
+use std::{collections::HashMap};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct DAG {
-    pub version: Version,
+    pub details: HashMap<String, serde_yaml::Value>,
 }
 
-#[derive(Debug,  Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Version {
+pub struct Detail {
     pub rule: serde_yaml::Value,
+    #[serde(default = "default_display_name")]
     pub display_name: String,
     pub description: String,
     pub required: bool,
-    pub message: Option<HashMap<String, String>>,
+    #[serde(default = "default_message")]
+    pub message: HashMap<String, String>,
+}
+
+fn default_message()->HashMap<String, String>{
+    HashMap::new()
+}
+
+fn default_display_name()->String{
+    String::from("")
+}
+
+
+impl DAG {
+    pub fn from_str(s: &str) -> Result<HashMap<String, Detail>, serde_yaml::Error> {
+        let mut res = HashMap::new();
+        let v: serde_yaml::Value = serde_yaml::from_str(s)?;
+        match v {
+            serde_yaml::Value::Mapping(v) => {
+                for (key, value) in v {
+                    match key {
+                        serde_yaml::Value::String(s) => {
+                            let b: Detail = serde_yaml::from_value(value)?;
+                            res.insert(s, b);
+                        }
+                        _ => println!("unkown type: {:?}", key),
+                    }
+                }
+            }
+            _ => println!("unkown type : {:?}", v),
+        }
+        Ok(res)
+    }
 }
 
 // use std::time::*;
